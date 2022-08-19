7bda4323a4356383a3ae292e8906855bf6673f76
Samuel Yoo <samueyoo@gmail.com>
2022-08-18 21:53:33 -0500

    Database connection via Knex implemented

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+require("dotenv").config();
+const { DATABASE_URL } = process.env
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: DATABASE_URL,
+  },
+
+
+};
