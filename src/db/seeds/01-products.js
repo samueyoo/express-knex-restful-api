2afbb1e2a7629f515fc65138f8523daea975a77a
Samuel Yoo <samueyoo@gmail.com>
2022-08-20 22:30:46 -0500

    Seed scripts updated to seed dummy fixture data files

--- src/db/seeds/01-products.js
+++ src/db/seeds/01-products.js
@@ -2,12 +2,13 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
+
+const products = require("../fixtures/products");
+
 exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  return knex
+    .raw("TRUNCATE TABLE products RESTART IDENTITY CASCADE")
+    .then(function () {
+      return knex("products").insert(products)
+    });
 };
--- src/db/seeds/03-products_categories.js
+++ src/db/seeds/03-products_categories.js
@@ -2,12 +2,13 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
+
+ const productsCategories = require("../fixtures/productsCategories");
+
 exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  return knex
+    .raw("TRUNCATE TABLE products_categories RESTART IDENTITY CASCADE")
+    .then(function () {
+      return knex("products_categories").insert(productsCategories);
+    });
 };
