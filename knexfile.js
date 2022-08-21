ea972afebc69380dfbce4f69100c62f9a50af2ed
Samuel Yoo <samueyoo@gmail.com>
2022-08-20 22:06:47 -0500

    Added migration files for initial tables; added seed scripts, need to still connect them to fixtures and update templates

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-
+const path = require('path'); //Added path module
 require("dotenv").config();
 const { DATABASE_URL } = process.env
 
@@ -11,6 +11,12 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: DATABASE_URL,
+    migrations: { //Added migrations property
+      directory: path.join(__dirname, 'src', 'db', 'migrations'), //Added directory property for migrations
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"), //Added directory for seeds
+    },
   },
 
 
