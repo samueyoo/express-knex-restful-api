e7e32ca00f2b9d8c6f07e4fa89c0e1d1cc034977
Samuel Yoo <samueyoo@gmail.com>
2022-08-22 18:08:49 -0500

    Additional summary endpoints involving joining tables with Lodash configuration implemented

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -4,7 +4,7 @@ const productsService = require("./products.service");
 async function productExists(req, res, next) {
   const product = await productsService.read(req.params.productId);
   if (product) {
-    res.locals.prodcut = product;
+    res.locals.product = product;
     return next();
   }
   next({ status: 404, message: `Product cannot be found,`});
@@ -16,11 +16,26 @@ function read(req, res, next) {
 }
 
 async function list(req, res) {
-  const data = await productsService.list();
+  const data = await productsService.list(); //Why are we not wrapping it in the data property
   res.json({ data });
+}
+
+async function listOutOfStockCount(req, res) {
+  res.json({ data: await productsService.listOutOfStockCount() }); //But we do wrap it here?
+}
+
+async function listPriceSummary(req, res) {
+  res.json({ data: await productsService.listPriceSummary() });
+}
+
+async function listTotalWeightByProduct(req, res) {
+  res.json({ data: await productsService.listTotalWeightByProduct() });
 }
 
 module.exports = {
   read: [asyncErrorBoundary(productExists), read],
   list: [asyncErrorBoundary(list)],
+  listOutOfStockCount: asyncErrorBoundary(listOutOfStockCount),
+  listPriceSummary: asyncErrorBoundary(listPriceSummary),
+  listTotalWeightByProduct: asyncErrorBoundary(listTotalWeightByProduct),
 };
