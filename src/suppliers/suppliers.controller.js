a4e685949b269ddcb12ea3c4c997ae79031caf1d
Samuel Yoo <samueyoo@gmail.com>
2022-08-21 22:38:55 -0500

    Seed scripts implemented; service modules added for each controller; controllers/routers implemented with all middleware validation

--- src/suppliers/suppliers.controller.js
+++ src/suppliers/suppliers.controller.js
@@ -1,17 +1,77 @@
-async function create(req, res, next) {
-  res.status(201).json({ data: { supplier_name: "new supplier" } });
+const suppliersService = require("./suppliers.service");
+const hasProperties = require("../errors/hasProperties");
+
+const hasRequiredProperties = hasProperties("supplier_name", "supplier_email");
+
+const VALID_PROPERTIES = [
+  "supplier_name",
+  "supplier_address_line_1",
+  "supplier_address_line_2",
+  "supplier_city",
+  "supplier_state",
+  "supplier_zip",
+  "supplier_phone",
+  "supplier_email",
+  "supplier_notes",
+  "supplier_type_of_goods",
+];
+
+function hasOnlyValidProperties(req, res, next) {
+  const { data = {} } = req.body;
+  const invalidFields = Object.keys(data).filter(field => !VALID_PROPERTIES.includes(field));
+
+  if (invalidFields.length) {
+    return next({
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
+    });
+  }
+  next();
+}
+
+function supplierExists(req, res, next) {
+  suppliersService
+    .read(req.params.supplierId)
+    .then(supplier => {
+      if (supplier) {
+        res.locals.supplier = supplier;
+        return next();
+      }
+      next({
+        status: 404,
+        message: `Supplier cannot be found.`,
+      });
+    })
+    .catch(next);
+}
+
+function create(req, res, next) {
+  suppliersService
+    .create(req.body.data)
+    .then(data => res.status(201).json({ data }))
+    .catch(next);
 }
 
 async function update(req, res, next) {
-  res.json({ data: { supplier_name: "updated supplier" } });
+  const updatedSupplier = { 
+    ...req.body.data, //Grabs all the properties of the requested body data
+    supplier_id: res.locals.supplier.supplier_id //Adds the supplier_id property, setting it to the supplier_id we found off of the currently existing supplier
+  };
+  suppliersService
+    .update(updatedSupplier)
+    .then(data => res.json({ data }))
+    .catch(next);
 }
 
 async function destroy(req, res, next) {
-  res.sendStatus(204);
+  suppliersService
+    .delete(res.locals.supplier.supplier_id)
+    .then(() => res.sendStatus(204))
+    .catch(next);
 }
 
 module.exports = {
-  create,
-  update,
-  delete: destroy,
+  create: [hasOnlyValidProperties, hasRequiredProperties, create],
+  update: [supplierExists, hasOnlyValidProperties, hasRequiredProperties, update],
+  delete: [supplierExists, destroy],
 };
